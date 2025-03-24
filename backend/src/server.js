6b21ad1d37ed1887293be14ca2e8b6aafb0084b6
Chimmani Madhu Varun <madhuvarun27c@gmail.com>
2025-03-24 13:45:37 +0530

    create route project structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 require("dotenv").config()
-require("../database")
+require("./database")
+const router = require("./routes/routes")
 
 const app = express();
 
+app.use("/api", router)
 app.listen(process.env.SERVER_PORT, () => {
     console.log("Server is running...")
 })
