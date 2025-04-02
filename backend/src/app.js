603f314237dd64e425e2b72c1e7e241114681cb9
Chimmani Madhu Varun <madhuvarun27c@gmail.com>
2025-04-02 18:07:43 +0530

    create basic express web server

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.json({ message: "Server is up and running..." });
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log("Server is up and running...");
+});
