11206814ab530e088bf61558791523178e2ca7e5
Chimmani Madhu Varun <madhuvarun27c@gmail.com>
2025-04-09 16:30:57 +0530

    add schema to database

--- backend/src/queries.sql
+++ backend/src/queries.sql
@@ -31,3 +31,61 @@ END IF;
 
 END;
 $$;
+
+create table amazon.categories
+(
+	id serial not null unique primary key,
+	name text
+);
+
+create table amazon.subcategories
+(
+	id serial not null,
+	name text,
+	category_id integer,
+	id),
+	foreign key(category_id) references categories(id)
+);
+
+create table amazon.producttypes (
+id serial primary key,
+name text,
+subcategory_id integer references amazon.subcategories(id)
+);
+
+create table amazon.filters (
+	id serial primary key,
+	producttypeid integer,
+	key text,
+	value text,
+	foreign key(producttypeid) references amazon.producttypes(id)
+);
+
+create table amazon.filter_options
+(
+id serial not null unique primary key,
+filter_id integer references amazon.filters(id),
+value text not null
+);
+
+
+create table amazon.products
+(
+id serial not null unique primary key,
+name text,
+description text,
+category_id integer references amazon.categories(id),
+subcategory_id integer references amazon.subcategories(id),
+product_type integer references amazon.producttypes(id),
+price numeric(10, 2),
+brand text,
+image_url text
+);
+
+create table amazon.product_attributes
+(
+	id serial not null unique primary key,
+	product_id integer references products(id),
+	key text not null,
+	value text not null
+);
