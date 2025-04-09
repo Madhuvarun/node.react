9649d76cbc9bed8b71ef45ac6054cf601ac03be8
Chimmani Madhu Varun <madhuvarun27c@gmail.com>
2025-04-09 16:34:26 +0530

    create app theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App.tsx";
+import theme from "./themes/themes.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ frontend/src/themes/themes.ts
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: { fontFamily: 'Georgia, " Times New Roman ", Times, serif ' },
+});
+
+export default theme;
